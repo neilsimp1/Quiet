52c733b4b53cf8c79320cde10306e9b1f8293671
Neil Simpson <neilsimp1@gmail.com>
2016-11-16 22:56:12 -0500

    Init commit

---
+++ src/Quiet/Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Quiet{
+    public class Profile {
+
+		public string Name { get; set; }
+		public string Username { get; set; }
+		public string Hostname { get; set; }
+		public string Group { get; set; }
+
+		public override string ToString() {
+			var sb = new StringBuilder();
+			sb.AppendLine($"Name:\t\t{Name}");
+			if(Group != null) sb.AppendLine($"Group:\t\t{Group}");
+			sb.AppendLine($"Hostname:\t{Hostname}");
+			if(Username != null) sb.AppendLine($"Username:\t{Username}");
+
+			return sb.ToString();
+		}
+	}
+}
--- src/Quiet/Program.cs
+++ src/Quiet/Program.cs
@@ -1,14 +1,72 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+using Newtonsoft.Json;
 
-namespace Quiet
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-        }
-    }
+namespace Quiet {
+	public class Program {
+
+		public static int Main(string[] args) {
+			var result = CommandLine.Parser.Default.ParseArguments<AddOptions, ListOptions>(args)
+			.MapResult(
+				(AddOptions options) => ExecuteAdd(options)
+				, (ListOptions options) => ExecuteList(options)
+				, errs => 1
+			);
+
+			Console.ReadLine();
+			return result;
+		}
+
+
+		private static int ExecuteAdd(AddOptions options) {
+			throw new NotImplementedException();
+		}
+
+		private static int ExecuteList(ListOptions options) {
+			var profiles = GetProfiles(options.Group);
+			foreach(var profile in profiles) Console.WriteLine(profile.ToString());
+
+			return 0;
+		}
+
+		private static IEnumerable<Profile> GetProfiles(string group) {
+			var json = File.ReadAllText("data/profiles.json");
+			var profiles = JsonConvert.DeserializeObject<IEnumerable<Profile>>(json);
+
+			if(group != null) {
+				profiles = profiles.Where(profile => {
+					var isInGroup = profile.Group == group;
+					profile.Group = null;
+					return isInGroup;
+				});
+			}
+
+			return profiles;
+		}
+
+
+		class AddOptions {
+
+			[Option('g', "group", Default = "default", HelpText = "Specify a profile group to list")]
+			public string Group { get; set; }
+
+		}
+
+		[Verb("list", HelpText = "List profiles")]
+		class ListOptions {
+
+			//// Save for telnet support?
+			//[Option('t', "type", HelpText = "Specify a profile type to list (`ssh`, `telnet`)")]
+			//public string Type { get; set; }
+
+			[Option('g', "group", HelpText = "Specify a profile group to list")]
+			public string Group { get; set; }
+
+		}
+
+	}
 }
